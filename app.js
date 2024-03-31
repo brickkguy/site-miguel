b2ee39b424546b9e59d85d0de91d5548ef02c3e6
brickkguy <pedro.ferreira.amorim.99@gmail.com>
2024-03-31 20:59:08 -0300

    let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') }

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (pontosDoMiguel <= 10) {
     alert('Miguel é um Sigma.');
 } else {
     alert('Miguel é um Bigma.');
-}
-if (pontosDoMiguel = 413, 69, 24, 101) {
+} else {
+    (pontosDoMiguel = 413, 69, 24, 101) 
     alert('Vish Margarina of the Player.');
 }
