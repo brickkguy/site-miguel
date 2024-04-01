8dd0743757cd6b17f7bebc139378115d606f45ff
brickkguy <pedro.ferreira.amorim.99@gmail.com>
2024-03-31 21:00:20 -0300

    let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') } let mindsetNumber = prompt('Digite um número.'); alert('Eu vou ler a sua mente'); alert('Se prepare...'); let resposta = (`Seu número era ${mindsetNumber}? (s/n)`); if (resposta == 's') {     alert('Tá tudo aqui, no meu mindset.'); } else {     alert('vai catar coquinho na famosa casa do meu mano vizinho.') }

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (pontosDoMiguel <= 10) {
     alert('Miguel é um Sigma.');
 } else {
     alert('Miguel é um Bigma.');
-} else {
-    (pontosDoMiguel = 413, 69, 24, 101) 
+}
+if (pontosDoMiguel = 413, 69, 24, 101) {
     alert('Vish Margarina of the Player.');
 }
