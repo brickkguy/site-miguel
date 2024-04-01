b236f34726d2cf032d3325ade6fdbad4410e062b
brickkguy <pedro.ferreira.amorim.99@gmail.com>
2024-03-31 21:01:15 -0300

    soimplemsnte o mgieil

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ if (pontosDoMiguel <= 10) {
     alert('Miguel é um Sigma.');
 } else {
     alert('Miguel é um Bigma.');
-}
-if (pontosDoMiguel = 413, 69, 24, 101) {
+} if (pontosDoMiguel = 413, 69, 24, 101) {
     alert('Vish Margarina of the Player.');
 }
